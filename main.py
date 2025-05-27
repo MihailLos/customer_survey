56aac947b8a6b7baccb3f486b7cdb218ddafa0ae
MihailLos <enderclub12@gmail.com>
2025-05-27 14:31:59 +0700

    saving answers in keys

--- main.py
+++ main.py
@@ -91,8 +91,7 @@ elif st.session_state.page == 1:
     st.markdown("### Вопрос 1. Как часто Вы покупаете пищевую продукцию?")
     selected = st.radio("", ["Каждый день", "Несколько раз в неделю", "Один раз в неделю", "Реже одного раза в неделю"], index=None)
     st.session_state["q1"] = selected
-    if st.button("Далее", disabled=check_required_question("q1")):
-        go_next()
+    st.button("Далее", on_click=go_next, disabled=check_required_question("q1"))
 
 elif st.session_state.page == 2:
     st.markdown("### Вопрос 2. Где чаще всего Вы приобретаете пищевую продукцию? (возможно указать несколько вариантов ответа)")
@@ -107,9 +106,12 @@ elif st.session_state.page == 2:
 
 elif st.session_state.page == 3:
     st.markdown("### Вопрос 3. Напишите, какие 3 элемента маркировки, по Вашему мнению, должны быть выделены наиболее заметно на упаковке любой пищевой продукции?")
-    e1 = st.text_input("Элемент 1:", key="q3_1")
-    e2 = st.text_input("Элемент 2:", key="q3_2")
-    e3 = st.text_input("Элемент 3:", key="q3_3")
+    e1 = st.text_input("Элемент 1:")
+    st.session_state["q3_1"] = e1
+    e2 = st.text_input("Элемент 2:")
+    st.session_state["q3_2"] = e2
+    e3 = st.text_input("Элемент 3:")
+    st.session_state["q3_3"] = e3
     st.button("Далее", on_click=go_next, disabled=not all([e1, e2, e3]))
 
 # MaxDiff пример — Вопрос 4
@@ -119,9 +121,11 @@ elif st.session_state.page == 4:
         options = ["Рекомендации и/или ограничения по использованию (в том числе по приготовлению)", 
                    "Количество (или объем) пищевой продукции", "Условия хранения", "Дата изготовления"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m1", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m1"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l1", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l1"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m1", "l1"))
 
 elif st.session_state.page == 5:
@@ -130,9 +134,11 @@ elif st.session_state.page == 5:
         options = ["Состав", "Срок годности", "Пищевая ценность (содержание или отсутствие определённых веществ)", 
                 "Наличие единого знака обращения продукции на рынке Евразийского экономического союза (EAC)"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m2", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m2"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l2", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l2"] = sel1
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m2", "l2"))
 
 elif st.session_state.page == 6:
@@ -142,9 +148,11 @@ elif st.session_state.page == 6:
                    "Наименование и местонахождение изготовителя", 
                    "Рекомендации и/или ограничения по использованию (в том числе по приготовлению)"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m3", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m3"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l3", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l3"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m3", "l3"))
 
 elif st.session_state.page == 7:
@@ -153,9 +161,11 @@ elif st.session_state.page == 7:
         options = ["Наименование и местонахождение изготовителя", "Условия хранения", "Срок годности", 
                    "Пищевая ценность (содержание или отсутствие определённых веществ)"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m4", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m4"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l4", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l4"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m4", "l4"))
 
 elif st.session_state.page == 8:
@@ -165,9 +175,11 @@ elif st.session_state.page == 8:
                    "Наличие единого знака обращения продукции на рынке Евразийского экономического союза (EAC)", 
                    "Состав", "Условия хранения"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m5", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m5"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l5", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l5"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m5", "l5"))
 
 elif st.session_state.page == 9:
@@ -176,9 +188,11 @@ elif st.session_state.page == 9:
         options = ["Пищевая ценность (содержание или отсутствие определённых веществ)", "Дата изготовления", 
                    "Рекомендации и/или ограничения по использованию (в том числе по приготовлению)", "Срок годности"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m6", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m6"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l6", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l6"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m6", "l6"))
 
 elif st.session_state.page == 10:
@@ -187,9 +201,11 @@ elif st.session_state.page == 10:
         options = ["Срок годности", "Наименование и местонахождение изготовителя", "Количество (или объем) пищевой продукции", 
                    "Состав"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m7", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m7"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l7", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l7"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m7", "l7"))
 
 elif st.session_state.page == 11:
@@ -199,9 +215,11 @@ elif st.session_state.page == 11:
                    "Пищевая ценность (содержание или отсутствие определённых пищевых веществ)", 
                    "Дата изготовления", "Наименование и местонахождение изготовителя"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m8", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m8"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l8", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l8"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m8", "l8"))
 
 elif st.session_state.page == 12:
@@ -211,35 +229,36 @@ elif st.session_state.page == 12:
                    "Наличие единого знака обращения продукции на рынке Евразийского экономического союза (EAC)", 
                    "Количество (или объем) пищевой продукции"]
         st.markdown('<div style="background-color:#e6f4ea;padding:10px;border-radius:5px;"><b>Наиболее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="m9", index=None)
+        sel1 = st.radio("", options, index=None)
+        st.session_state["m9"] = sel1
         st.markdown('<div style="background-color:#fdecea;padding:10px;border-radius:5px;"><b>Наименее важная информация</b></div>', unsafe_allow_html=True)
-        st.radio("", options, key="l9", index=None)
+        sel2 = st.radio("", options, index=None)
+        st.session_state["l9"] = sel2
         st.button("Далее", on_click=go_next, disabled=check_maxdiff_question("m9", "l9"))
 
 # --- Страница 3: Вопросы 13–19 ---
 elif st.session_state.page == 13:
     st.markdown("### Вопрос 13. Насколько Вы осведомлены о содержании нутриентов (белков, жиров, углеводов, витаминов и др.) в продукции, которую покупаете?")
-    st.radio("", ["Хорошо ориентируюсь", "Частично понимаю", "Знаю общие принципы", "Не обращаю внимания"], key="q13", 
-             index=None)
-
+    select = st.radio("", ["Хорошо ориентируюсь", "Частично понимаю", "Знаю общие принципы", "Не обращаю внимания"], index=None)
+    st.session_state["q13"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q13"))
 
 elif st.session_state.page == 14:
     st.markdown("### Вопрос 14. Насколько для Вас важна информация о пользе продукции для здоровья (например, содержание соли, сахара, холестерина)?")
-    st.radio("", ["Очень важно", "Скорее важно", "Не имеет значения"], key="q14", index=None)
-
+    select = st.radio("", ["Очень важно", "Скорее важно", "Не имеет значения"], index=None)
+    st.session_state["q14"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q14"))
 
 elif st.session_state.page == 15:
     st.markdown("### Вопрос 15. Часто ли Вы сталкиваетесь с трудностями при покупке продукции (например, мелкий шрифт на упаковке)?")
-    st.radio("", ["Да, регулярно", "Иногда", "Нет"], key="q15", index=None)
-
+    select = st.radio("", ["Да, регулярно", "Иногда", "Нет"], index=None)
+    st.session_state["q15"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q15"))
 
 elif st.session_state.page == 16:
     st.markdown("### Вопрос 16. Обращаете ли Вы внимание на наличие специализированной пищевой продукции (например, с пониженным содержанием сахара, безглютеновых, гипоаллергенных и др.)?")
-    st.radio("", ["Да, это важно", "Иногда обращаю внимание", "Нет, не обращаю внимание"], key="q16", index=None)
-
+    select = st.radio("", ["Да, это важно", "Иногда обращаю внимание", "Нет, не обращаю внимание"], index=None)
+    st.session_state["q16"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q16"))
 
 elif st.session_state.page == 17:
@@ -256,24 +275,28 @@ elif st.session_state.page == 17:
 
 elif st.session_state.page == 18:
     st.markdown("### Вопрос 18. Читаете ли Вы информацию на упаковке перед покупкой?")
-    st.radio("", ["Да, всегда внимательно изучаю", "Смотрю, но не углубляюсь", 
-              "Читаю только при выборе новой продукции", "Нет, не обращаю внимания"], key="q18", index=None)
+    select = st.radio("", ["Да, всегда внимательно изучаю", "Смотрю, но не углубляюсь", 
+              "Читаю только при выборе новой продукции", "Нет, не обращаю внимания"], index=None)
+    st.session_state["q18"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q18"))
 
 elif st.session_state.page == 19:
     st.markdown("### Вопрос 19. Хотели бы вы пользоваться цифровой системой, которая помогает выбирать пищевую продукцию на основе ваших предпочтений?")
-    st.radio("", ["Да", "Нет", "Затрудняюсь ответить"], key="q19", index=None)
+    select = st.radio("", ["Да", "Нет", "Затрудняюсь ответить"], index=None)
+    st.session_state["q19"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q19"))
 
 # --- Страница 4: Социально-демографический блок (20–27) ---
 elif st.session_state.page == 20:
     st.markdown("### Вопрос 20. Ваш пол")
-    st.radio("", ["Мужской", "Женский"], key="q20", index=None)
+    select = st.radio("", ["Мужской", "Женский"], index=None)
+    st.session_state["q20"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q20"))
 
 elif st.session_state.page == 21:
     st.markdown("### Вопрос 21. Ваша возрастная категория")
-    st.radio("", ["18–29 лет", "30–39 лет", "40–49 лет", "50–59 лет", "60 лет и выше"], key="q21", index=None)
+    select = st.radio("", ["18–29 лет", "30–39 лет", "40–49 лет", "50–59 лет", "60 лет и выше"], index=None)
+    st.session_state["q21"] = select
     st.button("Далее", on_click=go_next, disabled=check_required_question("q21"))
 
 elif st.session_state.page == 22:
@@ -287,34 +310,37 @@ elif st.session_state.page == 22:
 elif st.session_state.page == 23:
     st.markdown("### Вопрос 23. Ваш род занятий")
     selected = st.radio("", ["Учащийся / студент", "Работающий (наёмный)", "Работающий (Самозанятый / предприниматель)",
-              "Пенсионер", "Безработный", "Другое"], key="q23", index=None)
+              "Пенсионер", "Безработный", "Другое"], index=None)
+    st.session_state["q23"] = selected
     if selected == "Другое":
-        st.text_input("Уточните ваш род занятий:", key="q23_other")
+        text = st.text_input("Уточните ваш род занятий:")
+        st.session_state["q23_other"] = text
     st.button("Далее", on_click=go_next, disabled=check_other_required("q23", "q23_other"))
 
 elif st.session_state.page == 24:
     st.markdown("### Вопрос 24. Ваше семейное положение")
-    st.radio("", ["Состою в браке", "Не состою в браке"], key="q24", index=None)
-
+    selected = st.radio("", ["Состою в браке", "Не состою в браке"], index=None)
+    st.session_state["q24"] = selected
     st.button("Далее", on_click=go_next, disabled=check_required_question("q24"))
 
 elif st.session_state.page == 25:
     st.markdown("### Вопрос 25. Сколько человек проживает с Вами?")
-    st.radio("", ["Живу один", "1 человек", "2 человека", "3 человека", "4 человека", "5 и более"], key="q25", index=None)
-
+    selected = st.radio("", ["Живу один", "1 человек", "2 человека", "3 человека", "4 человека", "5 и более"], index=None)
+    st.session_state["q25"] = selected
     st.button("Далее", on_click=go_next, disabled=check_required_question("q25"))
 
 elif st.session_state.page == 26:
     st.markdown("### 26. Насколько комфортно Вам покрывать расходы на питание?")
-    st.radio("", ["Очень комфортно (не экономлю)", "Достаточно комфортно (иногда ищу скидки)", 
+    selected = st.radio("", ["Очень комфортно (не экономлю)", "Достаточно комфортно (иногда ищу скидки)", 
                   "Затруднительно (часто выбираю бюджетные варианты)", 
-                  "Очень сложно (вынужден(-а) сильно экономить на пищевой продукции)"], key="q26", index=None)
-
+                  "Очень сложно (вынужден(-а) сильно экономить на пищевой продукции)"], index=None)
+    st.session_state["q26"] = selected
     st.button("Далее", on_click=go_next, disabled=check_required_question("q26"))
 
 elif st.session_state.page == 27:
     st.markdown("### 27. Есть ли у Вас дети до 18 лет?")
-    st.radio("", ["Да", "Нет"], key="q27", index=None)
+    selected = st.radio("", ["Да", "Нет"], index=None)
+    st.session_state["q27"] = selected
 
     if st.button("Отправить анкету", disabled=check_required_question("q27")):
         answers = load_data.build_answers()
